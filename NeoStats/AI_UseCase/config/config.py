49921871ffc6f297bf216b7ae07eef4f20d0b533
Kamleshprajput <145245907+Kamleshprajput@users.noreply.github.com>
2026-03-13 21:54:21 +0530

    using secrets

--- NeoStats/AI_UseCase/config/config.py
+++ NeoStats/AI_UseCase/config/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 MODEL_NAME = "gemini-2.5-flash"
